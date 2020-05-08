b12bb8e79f22398ab67ceece0fb502dac27729a2
cawhitecode <casoccerdude@gmail.com>
2020-05-08 15:57:00 -0500

    Added some comments and default

--- src/app/components/dispatch/dispatch.component.ts
+++ src/app/components/dispatch/dispatch.component.ts
@@ -42,24 +42,24 @@ export class DispatchComponent implements OnInit {
     this.selectedItems = [{ item_id: 101, item_text: 'DFW - Dallas / Ft Worth' }]
     this.dropdownList = [
       { item_id: 1, item_text: 'ATL - Atlanta'},
-      { item_id: 100, item_text: 'CHI - Chicago' },
+      { item_id: 1000, item_text: 'CHI - Chicago' },
       { item_id: 2, item_text: 'CLE - Cleveland' },
-      { item_id: 101, item_text: 'DFW - Dallas / Ft Worth' },
+      { item_id: 1001, item_text: 'DFW - Dallas / Ft Worth' },
       { item_id: 3, item_text: 'CLS - Columbus' },
-      { item_id: 200, item_text: 'DEN - Denver' },
+      { item_id: 2000, item_text: 'DEN - Denver' },
       { item_id: 4, item_text: 'DET - Detroit' },
-      { item_id: 102, item_text: 'HOU - Houston' },
+      { item_id: 1002, item_text: 'HOU - Houston' },
       { item_id: 5, item_text: 'GRR - Grand Rapids' },
       { item_id: 6, item_text: 'MIA - Miami' },
       { item_id: 7, item_text: 'IND - Indianapolis' },
       { item_id: 8, item_text: 'NVA - Northern Virginia' },
-      { item_id: 103, item_text: 'MKE - Milwaukee' },
+      { item_id: 1003, item_text: 'MKE - Milwaukee' },
       { item_id: 104, item_text: 'MSP - Minneapolis / St Paul' },
       { item_id: 9, item_text: 'WPB - West Palm Beach' },
       { item_id: 10, item_text: 'NSH - Nashville' },
-      { item_id: 105, item_text: 'OKC - Oklahoma City' },
-      { item_id: 201, item_text: 'DEN - Denver' },
-      { item_id: 300, item_text: 'POR - Portland' }
+      { item_id: 1005, item_text: 'OKC - Oklahoma City' },
+      { item_id: 2001, item_text: 'DEN - Denver' },
+      { item_id: 3000, item_text: 'POR - Portland' }
     ];
     this.dropdownSettings = {
       singleSelection: false,
@@ -67,26 +67,34 @@ export class DispatchComponent implements OnInit {
       textField: 'item_text',
       selectAllText: 'Select All',
       unSelectAllText: 'UnSelect All',
-      itemsShowLimit: 3,
+      itemsShowLimit: 4,
       allowSearchFilter: true
     };
   }
   // setHours(d.getHours() - 2);
 
   onItemSelect(item: any) {
-    console.log(item);
     this.pushItemToSelectedMarkets(item);
   }
 
+  // Function to Add All markets to both Arrays
+ 
+
+  // On dropdown DeSelect finds index of item to be removed and removes from selected markets
   onItemDeSelect(item: any) {
     let marketToRemove = this.selectedMarkets.findIndex(x => x.marketId === item.item_id);
-    console.log(item.item_id);
-    console.log(marketToRemove);
     this.selectedMarkets.splice(marketToRemove, 1);
 
   }
 
-  // function to add a market to a 
+  // Function to remove all selected items from both Arrays
+  onAllItemDeSelect()
+  {
+    this.selectedItems = []
+    this.selectedMarkets = [];
+  }
+
+  // Function to add a market to selected Markets
   pushItemToSelectedMarkets(item){
     let newmarket = new Market();
     newmarket.marketId = item.item_id;
@@ -95,25 +103,22 @@ export class DispatchComponent implements OnInit {
     newmarket.map = "Map URL Goes Here";
     this.selectedMarkets.push(newmarket);
   }
-  // Function to set timezone based on item_id
+  // Function to set timezone based on item_id to be used in html to offset based on GMT
   setTimzoneBasedOnItemId(item_id: number, newmarket: Market){
-    if (item_id < 100){
+    if (item_id < 1000){
       newmarket.gmtOffset = '-4';
     }
-    else if (item_id < 200){
+    else if (item_id < 2000){
       newmarket.gmtOffset = '-5';
     }
-    else if (item_id < 300){
+    else if (item_id < 3000){
       newmarket.gmtOffset = '-6';
     }else if
-    (item_id < 400){
+    (item_id < 4000){
       newmarket.gmtOffset = '-7';
     }
     else {
       newmarket.gmtOffset = '-4';
     }
-  }
-  defaultMarket(){
-
   }
 }
